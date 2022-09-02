338462f7d5bd2d29d4cda55966354181e6ee3a69
sb3141 <100383822+sb3141@users.noreply.github.com>
2022-09-02 18:45:57 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,22 +1,12 @@
-import os
-from setuptools import setup
-
-version_contents = {}
-version_path = os.path.join(
-    os.path.abspath(os.path.dirname(__file__)), "taceconomics/version.py"
-)
-
-with open(version_path, "rt") as f:
-    exec(f.read(), version_contents)
-
+from setuptools import setup, find_packages
 
 setup(
     name="taceconomics",
+    version="0.1",
     description="Python client library for TAC ECONOMICS API",
-    version=version_contents["VERSION"],
-    install_requires=["requests", "pandas"],
     author="TAC ECONOMICS",
     author_email="info@taceconomics.com",
     url="https://github.com/taceconomics/taceconomics-python",
-    packages=["taceconomics"]
+    install_requires=["requests", "pandas"],
+    packages=find_packages()
 )
